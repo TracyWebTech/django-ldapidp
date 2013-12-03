7add904b4354e9c856da2c2ab1c1615cd98896f1
Sergio Oliveira <sergio@tracy.com.br>
2013-12-03 16:19:53 -0200

    Adding deps

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -16,3 +16,44 @@ ldap::define::domain {'puppetlabs.test':
   rootpw   => 'test',
   auth_who => 'anonymous'
 }
+
+# req to install python pkgs
+package { 'python-pip':
+  ensure => installed,
+}
+
+# req to create virtualenvs
+package { 'virtualenvwrapper':
+  ensure   => installed,
+  provider => pip,
+  require  => Package['python-pip'],
+}
+
+# req for any compilation
+package { 'build-essential':
+  ensure => installed,
+}
+
+# req for compiling every python pkg
+package { 'python-dev':
+  ensure => installed,
+}
+
+# req for compiling python-ldap
+package { 'libldap2-dev':
+  ensure => installed,
+}
+
+# req for compiling python-ldap
+package { 'libsasl2-dev':
+  ensure => installed,
+}
+
+# req for cloning code
+package { 'git':
+  ensure => installed,
+}
+
+package { 'mercurial':
+  ensure => installed,
+}
